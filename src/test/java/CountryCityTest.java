d41dbb952ebe71a2035f51c69f66a36097c45d79
MaryKaynak <56208212+MaryKaynak@users.noreply.github.com>
2020-03-03 00:19:43 -0500

    Initial commit

---
+++ src/test/java/CountryCityTest.java
@@ -0,0 +1,119 @@
+import Model.City;
+import io.restassured.http.ContentType;
+import Model.Country;
+import org.testng.annotations.Test;
+import utility.BaseTest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class CountryCityTest extends BaseTest {
+
+    private String CountryId;
+
+    @Test
+    public void getBasePath() {
+        given()
+                .when()
+                .log().body()
+                .get()
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void getCountries() {
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .log().body()
+                .get( "/school-service/api/countries" )
+                .then()
+                .statusCode( 200 )
+        ;
+    }
+    @Test
+    public void getCity() {
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .get( "/school-service/api/cities" )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+
+    @Test
+    public void createCountry() {
+        Country country = new Country();
+        country.setName( name );
+        country.setCode( code );
+
+        // creating country
+        CountryId = given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // deleting country
+//        given()
+//                .cookies( cookies )
+//                .when()
+//                .log().body()
+//                .delete( "/school-service/api/countries/" + countryId )
+//                .then()
+//                .log().body()
+//                .statusCode( 200 )
+//        ;
+    }
+    @Test (dependsOnMethods = {"createCountry"})
+    private void CreateCity(){
+        City city = new City();
+        city.setName(name);
+
+        Country country = new Country();
+        country.setId( CountryId );
+        city.setCountry( country );
+
+        given()
+                .cookies(cookies)
+                .body(city)
+                .log().body()
+                .contentType(ContentType.JSON)
+                .when()
+                .log().body()
+                .post("/school-service/api/cities")
+                .then()
+                .log().body()
+                .statusCode(201);
+//                .extract().jsonPath().getString("id");
+//        // deleting country
+//        given()
+//                .cookies( cookies )
+//                .when()
+//                .log().body()
+//                .delete( "/school-service/api/cities/" + city )
+//                .then()
+//                .log().body()
+//                .statusCode( 200 )
+//        ;
+    }
+
+
+
+}
---
+++ src/test/java/Model/City.java
@@ -0,0 +1,35 @@
+package Model;
+
+import utility.BaseTest;
+
+public class City extends BaseTest {
+    private String id;
+    private String name;
+    private Country country;
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
